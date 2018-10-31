43d336b4b56f2a2263c1bfe7e9fb230f8e3559e7
hugopinto <hugopinto@hotmail.com>
2018-10-31 00:00:25 +0000

    Adicção dos objetos de marcar faltas/presenças aos eventos Bkp da base de dados

---
+++ src/app/_models/SocioEvento.ts
@@ -0,0 +1,11 @@
+import { SocioEventoEstado } from "./SocioEventoEstado";
+
+
+export class SocioEvento {
+    idSocio: number;
+    idEvento: number;
+    estado:SocioEventoEstado;
+    obs:string
+}
+
+
---
+++ src/app/_models/SocioEventoEstado.ts
@@ -0,0 +1,5 @@
+export enum SocioEventoEstado {
+    Presente = 10,
+    Ausente=0,
+    MeiaPresenca = 5
+}
